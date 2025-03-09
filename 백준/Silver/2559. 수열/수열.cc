f74840e78271dc44655e597d1db376f8f27b7127
KIM 민찬 <163628956+banchan01@users.noreply.github.com>
2025-03-09 23:49:11 +0900

    [Silver III] Title: 수열, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,28 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,k, maxi=0,result=0;
+int n,k,tmp;
 int arr[100005];
+int result = -10000005;
 int main()
 {
     cin >> n >> k;
     for(int i=0; i<n; i++) {
-        int temp;
-        cin >> temp;
-        result += temp;
-        arr[i] = temp;
+        cin >> tmp;
+        arr[i+1] = arr[i] + tmp;
     }
-    for(int i=0; i<n;i++) {
-        if(i+k>n) break;
-        int tmp =0;
-        for(int j=i; j<i+k;j++) {
-            //cout << arr[j] << ' ';
-            tmp += arr[j];
-        }
-        if (maxi<tmp) maxi = tmp;
-        //cout << '\n';
+    for(int i=k; i<=n; i++) {
+        result = max(result, arr[i]-arr[i-k]);
     }
-    if (k!=n) result = maxi;
     cout << result;
     return 0;
 }
